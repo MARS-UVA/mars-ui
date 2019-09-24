7a1b1527dd6a7bff260df73c4f1ab4b323c2db53
hanzhi713 <hanzhi713@163.com>
2019-09-24 00:09:45 -0400

    add 09-24 pys

---
+++ 09-24/client-scratch.py
@@ -0,0 +1,18 @@
+# Echo client program
+import socket
+import random
+
+seq = [0xff, 0xff, 4, 23, 45, 67, 74, 0]
+seq[-1] = sum(seq[:-1]) % 255
+seq *= 10
+HOST = 'localhost'    # The remote host
+PORT = 6666              # The same port as used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    total = 0
+    while total < len(seq):
+        increment = random.randint(4, 10)
+        print("Sending", seq[total:total + increment])
+        s.sendall(bytes(seq[total:total + increment]))
+        print(s.recv(1024).decode("utf-8"))
+        total += increment

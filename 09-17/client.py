676746957ab6884548f83aa4037590f0b7692dba
hanzhi713 <hanzhi713@163.com>
2019-10-24 20:07:39 -0400

    add jetson folder

--- 09-17/client.py
+++ 09-17/client.py
@@ -1,27 +1,18 @@
 import socket
 
 HOST = '172.27.38.111'    # The remote host
-PORT = 6666              # The same port as used by the server
+PORT = 6667           # The same port as used by the server
 
 
-def send4int(a, b, c, d):
-    """
-    the four bytes between the header bytes and the checksum are the data bytes. 
-    Try to implement this function, send4int, that can send a, b, c, d, the parameters of this
-    function, as the data bytes to the server. Note that you need to compute the checksum
-    
-    For example, send4int(1, 2, 3, 4) should send [0xff, 0xff, 1, 2, 3, 4, 10]. 
-    You should get something like Received b'You send [255, 255, 1, 2, 3, 4, 10]\r\nThe checksum is correct'
-    """
-    seq = [0xff, 0xff, a, b, c, d, 0]
-    checksum = sum(seq[0:6]) % 256
-    seq[6] = checksum
+def sendints(*args):
+    seq = [] # make your sequence
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(bytes(seq * 2))
+        s.sendall(bytes(seq))
         print('Received', repr(s.recv(1024)))
 
 
-send4int(1, 2, 3, 5)
-send4int(127, 5, 19, 2)
+sendints(1, 2, 3, 5)
+sendints(127)
+sendints(1, 2, 3, 5, 9, 99, 123)
 
---
+++ jetson/serial_proto_test.py
@@ -0,0 +1,42 @@
+from serial_proto import var_len_proto_recv, var_len_proto_send
+from random import randint
+
+
+def gen_var_send_test(data: list):
+    count = len(data)
+    count = count | 0b11000000
+
+    buffer = [0xff, count]
+    buffer.extend(data)
+    buffer.append(sum(buffer) % 256)
+    return bytes(buffer)
+
+# ---- client test -----------------
+assert var_len_proto_send([1, 2, 3]) == [255, 195, 1, 2, 3, 200]
+for i in range(50):
+    count = randint(1, 16)
+    data = [randint(0, 255) for i in range(count)]
+    assert var_len_proto_send(data) == gen_var_send_test(data)
+         
+
+# ----- server test
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200])) == [1, 2, 3]
+for i in range(50):
+    count = randint(1, 16)
+    data = [randint(0, 255) for i in range(count)]
+    assert var_len_proto_recv(bytes(gen_var_send_test(data))) == data
+
+# --- incomplete data test
+assert var_len_proto_recv(bytes([254])) == []
+assert var_len_proto_recv(bytes([])) == []
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 199])) == [] # checksum mismatch
+assert var_len_proto_recv(bytes([255, 195])) == [] # segmented data
+assert var_len_proto_recv(bytes([1, 2, 3, 200])) == [1, 2, 3]
+assert var_len_proto_recv(bytes([255, 195, 2, 2, 3])) == []
+assert var_len_proto_recv(bytes([201])) == [2, 2, 3]
+assert var_len_proto_recv(bytes([255])) == []
+assert var_len_proto_recv(bytes([195])) == []
+assert var_len_proto_recv(bytes([2])) == []
+assert var_len_proto_recv(bytes([2, 3])) == []
+assert var_len_proto_recv(bytes([201])) == [2, 2, 3]
+assert var_len_proto_recv(bytes([255, 64, 1, 2, 3, 200])) == [] # count byte mismatch

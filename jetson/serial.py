83d23b2aacc2b890b7b8e20d6468f0b5308b5d25
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-11-17 12:12:00 -0500

    Working Serial Protocol Code

--- jetson/serial.py
+++ jetson/serial.py
@@ -5,89 +5,42 @@ indexOfNewData = 6      # Index of the value 255
 dataLength = 0      # Variable to check length of data
 placeholder = []        # Placeholder list used for converting.    
 
-def list_to_byte(data: list):
-    '''
-    @param: a list containing integers
-    @returns: a list with the byte format of the integers
-    '''
-    cs = len(data) | 192        # ...
-    seq = [255, cs] + data      # ...
-    seq += [sum(seq) % 256]     # ...
-    return bytes(seq)       # Returns the byte format of the list. 
 
-def byte_to_list(byteInput: list):
-    '''
-    @param: A list of containing numbers in byte format
-    @return: A list with specific values.
-    '''
-    global finalList        # A global final list
-    global currentList      # A global current list
-    global previousList     # A global previous list
-    global indexOfNewData       # A global variable for index
+temp = bytes()
 
-    data = list(byteInput)      # Changes the byte input to integers
-    currentList = []        # Resets the current list. 
-    finalList = []      # Resets the final list
 
-    if (len(data) == 0):        # If the length of the data is 0
-        previousList = []       # Reset the previous list 
-        return finalList        # Return whatever is in final list
+def var_len_proto_send(data: list):
+    count = len(data)
+    count = count | 0b11000000
 
-    if (len(data) > 6):     # If the length of the data is more than 6
-        dataLength = 0      # Reset the data length to 0 initially
-        dataLength = int(len(data)/6)       # Set the data length to be the length of the data found as a multiple of 6
-        currentList = data[0:6]     # Take the first 6 values in the list
-        placeholder = byte_to_list(currentList)     # Run the function specified with that specific list
-        return placeholder * dataLength     # Return the list found with the multiple in datalength
+    buffer = [0xff, count]
+    buffer.extend(data)
+    buffer.append(sum(buffer) % 256)
+    return bytes(buffer)
+    
+def var_len_proto_recv(data: bytes):
+    global temp
+    temp += data
+    output = []
+    while len(temp) > 2:  # read at least 3 bytes
+        for i in range(len(temp) - 2):
+            if temp[i] == 0xFF and temp[i + 1] & 0b11000000 == 0b11000000:  # check the header
+                count = temp[i + 1] & 0b111111
+                expected_len = 2 + count + 1  # 1 header + 1 count byte + ... + 1 checksum
+                # if temp's length is greater than the expected packet length
+                if len(temp) >= i + expected_len:
+                    slc = temp[i:i + expected_len]
 
-    elif (len(data) == 6):      # If the length of the data is 6
-        if (data[0] == 255):        # Check the first item, if the first item is 255
-            
-            total = 0       # Set total to be 0
-            checkSum = data[5]      # Find the checksum value in the overall list
+                    # remove bytes already read
+                    temp = temp[i + expected_len:]
 
-            for item in data:       # For every item in the data list
-                if item != checkSum:        # If the item is not the checksum value
-                    total += item       # Add the value to the total value
-
-            total = total % 256     # Reset total to be the value of the addition modulus 256
-            
-            if total == checkSum:       # If the total value is equal to checksum
-                finalList.append(data[2:5])     # Append the indexes specified to the final list
-
-            previousList = []       # Reset the previous list. 
-        else:
-            check_for_incomplete_data(data)     # Calls the function specified with an argument of the given data
-    else:
-        check_for_incomplete_data(data)     # Calls the function with an argument of the data
-
-    if (len(previousList) == 6):        # Checks if the length of the previous list is 6
-        byte_to_list(previousList)      # If it is, it calls the function on the previous list
-        currentList = []        # Resets the current list 
-
-    return list(finalList)      # Returns the values in the final list
-
-def check_for_incomplete_data(input2: list):
-    '''
-    @param: A list of containing numbers in byte formatg
-    @return: A list with specific values.
-    '''
-    global finalList        # A global final list
-    global currentList      # A global current list
-    global previousList     # A global previous list
-    global indexOfNewData       # A global variable for index
-
-    for item in input2:     # Checks each item in the list 
-        if (item == 255):       # If the item is the specified value
-            indexOfNewData = input2.index(255)      # The index of said value is saved
-            break       # The process stops. 
-        else:       # If the item is not the specified value
-            indexOfNewData = 6      # Set the index to be a random value 6 or above
-
-    for byte in input2:     # Checks each byte in the input
-        if indexOfNewData == 0:     # If the index of the new data is 0 (Checks if the value 255 is in the first index)
-            previousList.append(byte)       # Append the byte to the previous list
-        else:       # If the condition in the if statement fails
-            indexOfByte = input2.index(byte)        # Find the index of the byte
-            if indexOfByte < indexOfNewData:        # If the index of the byte is less than the index of the 255 value
-                previousList.append(byte)       # Append it to the previous list
+                    if sum(slc[:-1]) % 256 == slc[-1]:
+                        output.append(slc[2:-1])
+                    break
+                else:
+                    # not enough bytes
+                    temp = temp[i:]  # remove bytes already read
+                    return list(map(lambda x: list(x), output))
+        else:  # no single matching byte, remove all
+            temp = bytes()
+    return list(map(lambda x: list(x), output))
--- jetson/serial_proto_test.py
+++ jetson/serial_proto_test.py
@@ -1,5 +1,8 @@
-from serial import list_to_byte, byte_to_list
+from serial import var_len_proto_send, var_len_proto_recv
 from random import randint
+print(" ")
+print(" ")
+print(" ")
 
 def gen_var_send_test(data: list):
     count = len(data)
@@ -14,38 +17,38 @@ print("Starting Tests...")
 print(" ")
 
 # Client Test
-assert list_to_byte([1, 2, 3]) == bytes([255, 195, 1, 2, 3, 200])
+assert var_len_proto_send([1, 2, 3]) == bytes([255, 195, 1, 2, 3, 200])
 for i in range(50):
     count = randint(1, 16)
     data = [randint(0, 255) for i in range(count)]
-    assert list_to_byte(data) == gen_var_send_test(data)
+    assert var_len_proto_send(data) == gen_var_send_test(data)
 
 print("Client Test Passed ...")
-         
-# # Server test
-# assert byte_to_list(bytes([255, 195, 1, 2, 3, 200])) == [[1, 2, 3]]
-# for i in range(50):
-#     count = randint(1, 16)
-#     data = [randint(0, 255) for i in range(count)]
-#     assert byte_to_list(gen_var_send_test(data)) == [data]
 
-# print("Server Test Passed ...")
+# Server test
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200])) == [[1, 2, 3]]
+for i in range(50):
+    count = randint(1, 16)
+    data = [randint(0, 255) for i in range(count)]
+    assert var_len_proto_recv(gen_var_send_test(data)) == [data]
+
+print("Server Test Passed ...")
 
 # --- incomplete data test
-assert byte_to_list(bytes([254])) == []
-assert byte_to_list(bytes([])) == []
-assert byte_to_list(bytes([255, 195, 1, 2, 3, 199])) == [] # checksum mismatch
-assert byte_to_list(bytes([255, 195])) == [] # segmented data
-assert byte_to_list(bytes([1, 2, 3, 200])) == [[1, 2, 3]]
-assert byte_to_list(bytes([255, 195, 2, 2, 3])) == []
-assert byte_to_list(bytes([201])) == [[2, 2, 3]]
-assert byte_to_list(bytes([255])) == []
-assert byte_to_list(bytes([195])) == []
-assert byte_to_list(bytes([2])) == []
-assert byte_to_list(bytes([2, 3])) == []
-assert byte_to_list(bytes([201])) == [[2, 2, 3]]
-assert byte_to_list(bytes([255, 64, 1, 2, 3, 200])) == [] # count byte mismatch
-assert byte_to_list(bytes([255, 195, 1, 2, 3, 200] * 2)) == [[1, 2, 3]] * 2 # two packages all at once
+assert var_len_proto_recv(bytes([254])) == []
+assert var_len_proto_recv(bytes([])) == []
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 199])) == [] # checksum mismatch
+assert var_len_proto_recv(bytes([255, 195])) == [] # segmented data
+assert var_len_proto_recv(bytes([1, 2, 3, 200])) == [[1, 2, 3]]
+assert var_len_proto_recv(bytes([255, 195, 2, 2, 3])) == []
+assert var_len_proto_recv(bytes([201])) == [[2, 2, 3]]
+assert var_len_proto_recv(bytes([255])) == []
+assert var_len_proto_recv(bytes([195])) == []
+assert var_len_proto_recv(bytes([2])) == []
+assert var_len_proto_recv(bytes([2, 3])) == []
+assert var_len_proto_recv(bytes([201])) == [[2, 2, 3]]
+assert var_len_proto_recv(bytes([255, 64, 1, 2, 3, 200])) == [] # count byte mismatch
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200] * 2)) == [[1, 2, 3]] * 2 # two packages all at once
 
 print("Incomplete Data Test Passed ...")
 

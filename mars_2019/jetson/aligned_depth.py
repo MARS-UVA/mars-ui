f90c5cce6fb630fa98f716395e37ca64eca4d72c
hanzhi713 <hanzhi713@163.com>
2020-01-21 00:37:47 -0500

    rs aligned depth

---
+++ mars_2019/jetson/aligned_depth.py
@@ -0,0 +1,66 @@
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+#####################################################
+##              Align Depth to Color               ##
+#####################################################
+
+# First import the library
+import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
+import numpy as np
+# Import OpenCV for easy image rendering
+import cv2
+
+def generate_rgbd():
+    # Create a pipeline
+    pipeline = rs.pipeline()
+
+    #Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+    # Start streaming
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    print("Depth Scale is: " , depth_scale)
+
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 2 #1 meter
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    # Streaming loop
+    try:
+        while True:
+            # Get frameset of color and depth
+            frames = pipeline.wait_for_frames()
+            # frames.get_depth_frame() is a 640x360 depth image
+
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
+
+            # Get aligned frames
+            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
+
+            # Validate that both frames are valid
+            if not aligned_depth_frame or not color_frame:
+                continue
+
+            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+            yield depth_image, color_image
+    finally:
+        pipeline.stop()
--- mars_2019/jetson/seg.py
+++ mars_2019/jetson/seg.py
@@ -1,5 +1,6 @@
 import cv2
 from ..utils.video_stream import start_stream
+from .aligned_depth import generate_rgbd
 
 import jetson.inference
 import jetson.utils
@@ -12,7 +13,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Segment a live camera stream using an semantic segmentation DNN.", 
                                  formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.segNet.Usage())
 
-parser.add_argument("--network", type=str, default="fcn-resnet18-voc", help="pre-trained model to load, see below for options")
+parser.add_argument("--network", type=str, default="fcn-resnet18-cityscapes-1024x512", help="pre-trained model to load, see below for options")
 parser.add_argument("--width", type=int, default=1280, help="desired width of camera stream (default is 1280 pixels)")
 parser.add_argument("--height", type=int, default=720, help="desired height of camera stream (default is 720 pixels)")
 
@@ -25,26 +26,26 @@ except:
 
 net = jetson.inference.segNet(opt.network, sys.argv)
 net.SetOverlayAlpha(255)
-print(type(net.MaskClass))
 
 width = opt.width
 height = opt.height
 classes = jetson.utils.cudaAllocMapped(width * height)
-camera = jetson.utils.gstCamera(width, height, "/dev/video0")
 
 def frame_generator():
-
     downscale = 2
 
     out_width = opt.width // downscale
     out_height = opt.height // downscale
 
-    output = np.zeros((out_height * 2, out_width, 3), dtype=np.uint8)
+    output = np.zeros((out_height, out_width * 3, 3), dtype=np.uint8)
+    rgbd_generator = generate_rgbd()
+    rgba_img = np.ones((height, width, 4), dtype="float32") * 255
     while True:
-        # process the segmentation network
-        img, width, height = camera.CaptureRGBA(zeroCopy=1)
+        depth_img, color_img = next(rgbd_generator)
+        rgba_img[:, :, :3] = color_img
 
-        net.Process(img, width, height, "void")
+        # net takes in float32 RGBA
+        net.Process(jetson.utils.cudaFromNumpy(rgba_img), width, height, "void")
         net.MaskClass(classes, out_width, out_height)
 
         # cudaToNumpy assumes float32. need to divide by 8 to account for uint8
@@ -52,13 +53,13 @@ def frame_generator():
         mask_np = mask_np.view('uint8').reshape(out_height, out_width)
 
         output.fill(0)
-        output[:out_height, :, :][
-            ((mask_np == 2) | (mask_np == 3) | (mask_np == 4) | (mask_np == 12))
+        # color the ground with white color.
+        output[:, :out_width, :][
+            ((mask_np == 2) | (mask_np == 3) | (mask_np == 4) | (mask_np == 12)) #
         ] = (255, 255, 255)
 
-        img_np = jetson.utils.cudaToNumpy(img, width, height, 4).astype('uint8') # convert original img to numpy
-        img_np = cv2.resize(img_np, (out_width, out_height)) # resize
-        cv2.cvtColor(img_np, cv2.COLOR_RGBA2BGR, dst=output[out_height:, :, :])
+        output[:, out_width:2 * out_width, :] = cv2.resize(color_img, (out_width, out_height))
+        output[:, 2 * out_width:, :] = cv2.resize(depth_img, (out_width, out_height))[:, :, np.newaxis]
 
         yield output
 

15c48d0b361f17eeda71eb6ad5ab4016391743c7
hanzhi713 <hanzhi713@163.com>
2020-02-04 23:30:27 -0500

    add simply rpc client implementation

---
+++ mars_2019/laptop/README.md
@@ -0,0 +1,18 @@
+# Laptop Package
+
+## RPC Client
+
+First, make sure you have grpc installed
+
+```bash
+pip install grpcio grpcio-tools
+```
+
+To use the rpc client, copy the [jetsonroc.proto file](https://github.com/hanzhi713/mars-ros/blob/master/src/rpc-server/jetsonrpc.proto) from the mars-ros repository, and then compile the protocol definitions
+
+```bash
+cd mars_2019/laptop
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. jetsonrpc.proto
+```
+
+
---
+++ mars_2019/laptop/rpc_client.py
@@ -0,0 +1,42 @@
+import grpc
+import cv2
+import numpy as np
+
+from . import jetsonrpc_pb2_grpc
+from . import jetsonrpc_pb2
+import typing
+import struct
+
+STUB = jetsonrpc_pb2_grpc.JetsonRPCStub
+
+
+def stream_image(stub: STUB):
+    response = stub.StreamImage(jetsonrpc_pb2.Void())
+    for item in response:
+        arr = np.frombuffer(item.data, "uint8")
+        yield cv2.imdecode(arr, cv2.IMREAD_COLOR)
+
+
+def stream_imu(stub: STUB):
+    response = stub.StreamIMU(jetsonrpc_pb2.Void())
+    for item in response:
+        yield item.values
+
+
+def stream_motor_current(stub: STUB):
+    response = stub.StreamMotorCurrent(jetsonrpc_pb2.Void())
+    for item in response:
+        yield struct.unpack('8B', item.values)
+
+
+def send_motor_cmd(stub: STUB, gen):
+    return stub.StreamMotorCurrent(gen)
+
+
+if __name__ == '__main__':
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = jetsonrpc_pb2_grpc.JetsonRPCStub(channel)
+
+        data = stream_imu(stub)
+        for item in data:
+            print(item)

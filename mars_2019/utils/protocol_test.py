a208e40b9a66029d3fad02de265e8d6d8652f24e
Hanzhi Zhou <hanzhi713@163.com>
2020-02-17 19:25:22 -0500

    more efficient protocol

--- mars_2019/utils/protocol_test.py
+++ mars_2019/utils/protocol_test.py
@@ -11,34 +11,35 @@ def gen_var_send_test(data: list):
     buffer.append(sum(buffer) % 256)
     return bytes(buffer)
 
+
 # ---- client test -----------------
 assert var_len_proto_send([1, 2, 3]) == bytes([255, 195, 1, 2, 3, 200])
 for i in range(50):
     count = randint(1, 16)
     data = [randint(0, 255) for i in range(count)]
     assert var_len_proto_send(data) == gen_var_send_test(data)
-         
+
 
 # ----- server test
-assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200])) == [[1, 2, 3]]
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200])) == [bytes([1, 2, 3])]
 for i in range(50):
     count = randint(1, 16)
     data = [randint(0, 255) for i in range(count)]
-    assert var_len_proto_recv(gen_var_send_test(data)) == [data]
+    assert var_len_proto_recv(gen_var_send_test(data)) == [bytes(data)]
 
 # --- incomplete data test
 assert var_len_proto_recv(bytes([254])) == []
 assert var_len_proto_recv(bytes([])) == []
-assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 199])) == [] # checksum mismatch
-assert var_len_proto_recv(bytes([255, 195])) == [] # segmented data
-assert var_len_proto_recv(bytes([1, 2, 3, 200])) == [[1, 2, 3]]
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 199])) == []  # checksum mismatch
+assert var_len_proto_recv(bytes([255, 195])) == []  # segmented data
+assert var_len_proto_recv(bytes([1, 2, 3, 200])) == [bytes([1, 2, 3])]
 assert var_len_proto_recv(bytes([255, 195, 2, 2, 3])) == []
-assert var_len_proto_recv(bytes([201])) == [[2, 2, 3]]
+assert var_len_proto_recv(bytes([201])) == [bytes([2, 2, 3])]
 assert var_len_proto_recv(bytes([255])) == []
 assert var_len_proto_recv(bytes([195])) == []
 assert var_len_proto_recv(bytes([2])) == []
 assert var_len_proto_recv(bytes([2, 3])) == []
-assert var_len_proto_recv(bytes([201])) == [[2, 2, 3]]
-assert var_len_proto_recv(bytes([255, 64, 1, 2, 3, 200])) == [] # count byte mismatch
+assert var_len_proto_recv(bytes([201])) == [bytes([2, 2, 3])]
+assert var_len_proto_recv(bytes([255, 64, 1, 2, 3, 200])) == []  # count byte mismatch
 
-assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200] * 2)) == [[1, 2, 3]] * 2 # two packages all at once
+assert var_len_proto_recv(bytes([255, 195, 1, 2, 3, 200] * 2)) == [bytes([1, 2, 3])] * 2  # two packages all at once

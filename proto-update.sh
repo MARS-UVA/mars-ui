639539facbe6f89925049d2a63ddb326a5ddef4a
hanzhi713 <hanzhi713@163.com>
2020-02-20 20:07:07 -0500

    update script

--- proto-update.sh
+++ proto-update.sh
@@ -3,6 +3,6 @@
 mkdir -p mars_2019/laptop/protos
 cd mars_2019/laptop/protos
 rm jetsonrpc.proto
-wget https://github.com/hanzhi713/mars-ros/blob/master/src/rpc-server/jetsonrpc.proto
+wget https://raw.githubusercontent.com/hanzhi713/mars-ros/master/src/rpc-server/jetsonrpc.proto
 cd ..
 python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. protos/jetsonrpc.proto
